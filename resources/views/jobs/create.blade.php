5c8400bdb025efa7c6c30413bbaa4753828be7d6
mejba13 <mejba.13@gmail.com>
2024-06-16 15:56:24 +0600

    Make a Login and Registration System From Scratch: Part 1

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -11,43 +11,28 @@
                 <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful details from you</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title">Job Title</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift Leader" required>
-                            </div>
-                            @error('title')
-                                <p class="text-xs text-red-500 font-semibold">{{ $message }}</p>
-                            @enderror
+                           <x-form-input name="title" id="title" placeholder="CEO" required/>
+                           <x-form-error name="title" />
                         </div>
-                    </div>
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
+                    </x-form-field>
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 per years" required>
-                            </div>
-                            @error('salary')
-                                <p class="text-xs text-red-500 font-semibold">{{ $message }}</p>
-                            @enderror
+                            <x-form-input name="salary" id="title" placeholder="$50,000" required/>
+                            <x-form-error name="salary" />
                         </div>
-                    </div>
+                    </x-form-field>
                 </div>
-
-{{--                @if($errors->any())--}}
-{{--                    @foreach($errors->all() as $error)--}}
-{{--                        <li class="py-2 text-red italic">{{ $error }}</li>--}}
-{{--                    @endforeach--}}
-{{--                @endif--}}
             </div>
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <x-form-button> Save </x-form-button>
         </div>
     </form>
-
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
@@ -18,5 +20,13 @@ Route::view('/', 'home');
 //});
 
 Route::resource('jobs', JobController::class);
+
+//Auth
+
+Route::get('/register',[RegisterUserController::class,'create']);
+Route::post('/register',[RegisterUserController::class,'store']);
+
+Route::get('/login',[SessionController::class,'create']);
+Route::post('/login',[SessionController::class,'store']);
 
 Route::view('/contact','contact');
