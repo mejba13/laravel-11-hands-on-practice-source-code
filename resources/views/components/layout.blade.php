7c48e441971be262f1fe074290f432bd8ba0c89a
mejba13 <mejba.13@gmail.com>
2024-06-18 01:45:01 +0600

    get your build process in order

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,14 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ $headings }} - Laracasts - 30 Days to Learn Laravel </title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    {{-- Twilwindcss CDN --}}
-
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
 
 </head>
 
